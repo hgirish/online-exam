ffac85362c27ea7f8029ca79afc1cc97d4756aad
hgirish <girish@post.com>
2018-04-04 18:34:27 -0700

    adding SqlAlchemy and some entities

--- backend/src/main.py
+++ backend/src/main.py
@@ -17,4 +17,4 @@ if len(exams) == 0:
 
 print('### Exams:')
 for exam in exams:
-  print(f('({exam.id}) {exam.title} - {exam.description}'))
+  print(f'({exam.id}) {exam.title} - {exam.description}')
