397fae118fc4579f1faee344947bdc470ebfb3b8
hgirish <girish@post.com>
2018-04-04 20:43:15 -0700

    added VScode workspace

--- backend/bootstrap.ps1
+++ backend/bootstrap.ps1
@@ -1,6 +1,6 @@
 $env:FLASK_APP = "./src/main.py"
 $venv = pipenv --venv
-$cmd = $venv + "/Scripts/activate"
+$cmd = $venv + "/Scripts/Activate.ps1"
 Invoke-Expression $cmd
 #source $(pipenv --venv)/bin/activate
 flask run -h 0.0.0.0
